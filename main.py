02f6ce5bfae98603866c37f6003b007c3328cf46
Alexander Denton <100879026+Atden04@users.noreply.github.com>
2025-07-08 14:17:00 +0100

    updated Template code

--- main.py
+++ main.py
@@ -1,34 +1,45 @@
-# Task 2 - Please turn right
 def turn_right_90():
-    # Add code here
-    basic.pause(450)
+    Kitronik_Move_Motor.motor_on(Kitronik_Move_Motor.Motors.MOTOR_LEFT,
+            Kitronik_Move_Motor.MotorDirection.FORWARD,
+            speed)
+    Kitronik_Move_Motor.motor_on(Kitronik_Move_Motor.Motors.MOTOR_RIGHT,
+        Kitronik_Move_Motor.MotorDirection.REVERSE,
+        speed)
+    basic.pause(turnTime)
     Kitronik_Move_Motor.stop()
 
-# Task 3 - Now turn left
+# Task 2 - Add function to turn left
 
 
-# Task 5 - Move forward
+# This functions tells the MicroMouse to move forward until it's a specifiec
+# distance from the wall, this value is stored in 'distanceToWall'.
 def move_to_wall():
     while Kitronik_Move_Motor.measure() > distanceToWall:
-        # Add Code here
+        Kitronik_Move_Motor.motor_on(Kitronik_Move_Motor.Motors.MOTOR_LEFT,
+                    Kitronik_Move_Motor.MotorDirection.FORWARD,
+                    speed)
+        Kitronik_Move_Motor.motor_on(Kitronik_Move_Motor.Motors.MOTOR_RIGHT,
+            Kitronik_Move_Motor.MotorDirection.FORWARD,
+            speed)
         basic.pause(100)
     Kitronik_Move_Motor.stop()
 
 # Task 6 - Escape the maze!
+def navigate_maze():
+    # Add you functions calls here to navigate the maze
 
+    pass  
 
 # DO NOT touch this code - it is needed to stop the mouse from veering to the side.
 biasValue = 3
 Kitronik_Move_Motor.motor_balance(Kitronik_Move_Motor.SpinDirections.RIGHT, biasValue)
 Kitronik_Move_Motor.set_ultrasonic_units(Kitronik_Move_Motor.Units.CENTIMETERS)
 
-
 # Add your variables for speed and distance to wall here
+speed = 0
+turnTime = 0
 distanceToWall = 0
 
-# Call your function to escape the maze
 
-
-# Task 4 - Object ahead!
-def on_forever():
-    # Add code here
+# Calls function to exit the maze
+navigate_maze()
--- main.ts
+++ main.ts
@@ -1,7 +1,35 @@
-//  Task 2 - Please turn right
 function turn_right_90() {
-    //  Add code here
-    basic.pause(450)
+    Kitronik_Move_Motor.motorOn(Kitronik_Move_Motor.Motors.MotorLeft, Kitronik_Move_Motor.MotorDirection.Forward, speed)
+    Kitronik_Move_Motor.motorOn(Kitronik_Move_Motor.Motors.MotorRight, Kitronik_Move_Motor.MotorDirection.Reverse, speed)
+    basic.pause(turnTime)
     Kitronik_Move_Motor.stop()
 }
 
+//  Task 2 - Add function to turn left
+//  This functions tells the MicroMouse to move forward until it's a specifiec
+//  distance from the wall, this value is stored in 'distanceToWall'.
+function move_to_wall() {
+    while (Kitronik_Move_Motor.measure() > distanceToWall) {
+        Kitronik_Move_Motor.motorOn(Kitronik_Move_Motor.Motors.MotorLeft, Kitronik_Move_Motor.MotorDirection.Forward, speed)
+        Kitronik_Move_Motor.motorOn(Kitronik_Move_Motor.Motors.MotorRight, Kitronik_Move_Motor.MotorDirection.Forward, speed)
+        basic.pause(100)
+    }
+    Kitronik_Move_Motor.stop()
+}
+
+//  Task 6 - Escape the maze!
+function navigate_maze() {
+    //  Add you functions calls here to navigate the maze
+    
+}
+
+//  DO NOT touch this code - it is needed to stop the mouse from veering to the side.
+let biasValue = 3
+Kitronik_Move_Motor.motorBalance(Kitronik_Move_Motor.SpinDirections.Right, biasValue)
+Kitronik_Move_Motor.setUltrasonicUnits(Kitronik_Move_Motor.Units.Centimeters)
+//  Add your variables for speed and distance to wall here
+let speed = 0
+let turnTime = 0
+let distanceToWall = 0
+//  Calls function to exit the maze
+navigate_maze()
