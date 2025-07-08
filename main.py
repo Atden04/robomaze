fe96d0e4980a75e3ede47a76f5db2b326078c722
Alexander Denton <100879026+Atden04@users.noreply.github.com>
2025-07-08 11:30:00 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 # Task 2 - Please turn right
 def turn_right_90():
     # Add code here
+    Kitronik_Move_Motor.motor_on(Kitronik_Move_Motor.Motors.MOTOR_LEFT,
+            Kitronik_Move_Motor.MotorDirection.FORWARD,
+            speed)
+    Kitronik_Move_Motor.motor_on(Kitronik_Move_Motor.Motors.MOTOR_RIGHT,
+        Kitronik_Move_Motor.MotorDirection.REVERSE,
+        speed)
     basic.pause(450)
     Kitronik_Move_Motor.stop()
 
@@ -11,11 +17,19 @@ def turn_right_90():
 def move_to_wall():
     while Kitronik_Move_Motor.measure() > distanceToWall:
         # Add Code here
+        Kitronik_Move_Motor.motor_on(Kitronik_Move_Motor.Motors.MOTOR_LEFT,
+                    Kitronik_Move_Motor.MotorDirection.FORWARD,
+                    speed)
+        Kitronik_Move_Motor.motor_on(Kitronik_Move_Motor.Motors.MOTOR_RIGHT,
+            Kitronik_Move_Motor.MotorDirection.FORWARD,
+            speed)
         basic.pause(100)
     Kitronik_Move_Motor.stop()
 
 # Task 6 - Escape the maze!
-
+def navigate_maze():
+    turn_right_90()
+    turn_right_90()
 
 # DO NOT touch this code - it is needed to stop the mouse from veering to the side.
 biasValue = 3
@@ -26,10 +40,6 @@ Kitronik_Move_Motor.set_ultrasonic_units(Kitronik_Move_Motor.Units.CENTIMETERS)
 distanceToWall = 0
 
 # Call your function to escape the maze
+speed = 20
 
-
-# Task 4 - Object ahead!
-def on_forever():
-    # Add code here
-
-basic.forever(on_forever)
+navigate_maze()
